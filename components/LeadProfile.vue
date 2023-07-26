e03c3242056d0a02ba8be693daaee4c29cc6fc2f
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-07-26 17:35:08 -0400

    feat: lead profiles

---
+++ components/LeadProfile.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps(["name"]);
+
+const known_names = [
+    "ayden_bourdeau",
+    "jonas_hill",
+    "kaden",
+    "mahsam",
+];
+
+const profile_picture_url = computed(() => {
+    let cleaned_name = props.name.replace(" ", "_").toLowerCase();
+    if (!known_names.includes(cleaned_name)) {
+        console.warn(`Unknown name: ${props.name}`);
+        cleaned_name = "unknown";
+    }
+    return `/media/team_members/${cleaned_name}.jpg`;
+});
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="profile_picture_url" class="profile_picture"/>
+        <h2>{{ props.name }}</h2>
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    padding: 10px;
+    background-color: var(--gryphon-white);
+    color: var(--gryphon-black);
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.profile_picture {
+    width: 200px;
+    height: 200px;
+    image-rendering: crisp-edges;
+    /* image-rendering: -webkit-optimize-contrast; */
+    object-fit: cover;
+    border-radius: 10px;
+}
+
+h1 {
+    padding: 0;
+}
+</style>
--- pages/historical.vue
+++ pages/historical.vue
@@ -1,3 +1,23 @@
+<script setup>
+import LeadProfile from "@/components/LeadProfile.vue";
+</script>
+
 <template>
     <div>historical</div>
+    <div class="profiles_list">
+        <LeadProfile name="Ayden Bourdeau" />
+        <LeadProfile name="Jonas Hill" />
+        <LeadProfile name="Kaden" />
+        <LeadProfile name="Mahsam" />
+        <LeadProfile name="Bernardo" />
+    </div>
 </template>
+
+<style scoped>
+.profiles_list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+</style>
