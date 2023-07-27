4f5656ca75018691f5e7a02fe3ed7c13bd56116d
ChrisTienYenWalker <christienyenwalker@gmail.com>
2023-07-27 11:38:51 -0400

    Updated the Footer and Contact Page

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+import { createApp } from 'vue'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
+const app = createApp(App);
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'YOUR_API_KEY_COMES_HERE',
+    },
+}).mount('#app')
+
+
+
 <template>
     <NavBar />
     <NuxtPage />
