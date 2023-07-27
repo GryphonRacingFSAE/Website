b334d3c7ec0910eb2bc71df7606f967f2adfb260
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-07-27 01:29:19 -0400

    fix: building for vite-ssg

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            component: HomeView,
-        },
-        {
-            path: "/contact",
-            component: () => import("@/views/ContactView.vue"),
-        },
-        {
-            path: "/team",
-            component: () => import("@/views/TeamView.vue"),
-        },
-        {
-            path: "/cars",
-            component: () => import("@/views/CarsView.vue"),
-        },
-        {
-            path: "/projects",
-            component: () => import("@/views/ProjectsView.vue"),
-        },
-        {
-            path: "/sponsors",
-            component: () => import("@/views/SponsorView.vue"),
-        },
-    ],
-});
-
-export default router;
+export const routes = [
+    {
+        path: "/",
+        component: HomeView,
+    },
+    {
+        path: "/contact",
+        component: () => import("@/views/ContactView.vue"),
+    },
+    {
+        path: "/team",
+        component: () => import("@/views/TeamView.vue"),
+    },
+    {
+        path: "/cars",
+        component: () => import("@/views/CarsView.vue"),
+    },
+    {
+        path: "/projects",
+        component: () => import("@/views/ProjectsView.vue"),
+    },
+    {
+        path: "/sponsors",
+        component: () => import("@/views/SponsorView.vue"),
+    },
+];
