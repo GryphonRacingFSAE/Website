2593a7415a801061bcece9fb69652ac2fdbe735e
ChrisTienYenWalker <christienyenwalker@gmail.com>
2023-07-27 23:05:13 -0400

    deleted nuxt dir

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/walke/OneDrive/Documents/Coding/GitHub/Website/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
