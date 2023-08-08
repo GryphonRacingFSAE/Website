ee3f19a00404eb0b4b55871aa1db0b50784c276c
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-08 13:11:12 -0400

    feat: opengraph preview

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterView } from "vue-router";
 import NavBar from "@/components/NavBar.vue";
 import NavFooter from "@/components/NavFooter.vue";
 </script>
+
 <template>
     <NavBar />
     <RouterView />
