d2a8b2b5d637781b256589373f30e122bb656208
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-08 13:28:40 -0400

    fix: open graph image url

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,25 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig, loadEnv, type ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv) => {
     const env = loadEnv(mode, process.cwd());
 
+    env.VITE_BASE_URL ??= "/";
+
     return {
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        VITE_BASE_URL: env.VITE_BASE_URL,
+                    },
+                },
+            }),
+        ],
         resolve: {
             alias: {
                 "@": fileURLToPath(new URL("./src", import.meta.url)),
